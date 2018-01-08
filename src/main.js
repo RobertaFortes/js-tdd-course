f51d6074a25184580ef09e922d326b4f8c7f27f1
Roberta Fortes <robertafortes@Robertas-Mac-mini.local>
2018-01-08 16:34:18 -0200

    teste

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-a = 3
-const b = 2;
-const b = 2;
+const a = 3;
+const b = 2
+
 
 const c = 'jgdjhagdjabkbfakjbflbFwbeflwBFLkwebfkjhslbfljshkdbfjlsbfhjsBJsbVhkajsmBhjsbdcvjhsBhsdbxksbxvhjdSLbvhjbsedfhljsBzjbkfds';
 
